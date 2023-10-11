41efca53e59bb3a0ac00f0a7b0307c3d98af5b33
satoshi-komatsu <k023g1069@g.neec.ac.jp>
2023-10-11 10:34:10 +0900

    前期と後期で名前を変えた

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("GC1C_08_komatsu");
+Console.WriteLine("1年前期_GC1C_06_komatsu");
+
+Console.WriteLine("1年後期_GC1C_08_komatsu");
